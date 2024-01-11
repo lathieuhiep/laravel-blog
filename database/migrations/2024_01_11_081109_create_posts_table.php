70ce56fd0028e8f24d6c9c0359ab7c793f57bbbe
lathieuhiep <khacdiepkma90@gmail.com>
2024-01-11 17:08:54 +0700

    update code

--- database/migrations/2024_01_11_081109_create_posts_table.php
+++ database/migrations/2024_01_11_081109_create_posts_table.php
@@ -11,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->string('slug')->unique();
+            $table->text('content')->nullable();
+            $table->string('excerpt')->nullable();
+            $table->char('status', 20);
             $table->softDeletes();
             $table->timestamps();
         });
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
